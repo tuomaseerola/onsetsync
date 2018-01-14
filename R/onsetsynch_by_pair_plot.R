cea8a32d02ffe690332b761680bbb49b7c8c19ad
tuomaseerola <tuomas.eerola@kotikone.fi>
2018-01-14 20:06:26 +0000

    edit

--- R/onsetsynch_by_pair_plot.R
+++ R/onsetsynch_by_pair_plot.R
@@ -29,9 +29,7 @@ onsetsynch_by_pair_plot <-function(df,bybeat=FALSE){
 
   }
   if(bybeat==TRUE){
-    str(df)
-    head(df$asynch)
-    m<-reshape2::melt(df$asynch,id.vars='Instrument')
+    m<-reshape2::melt(df$asynch)
     m$value<-m$value*1000
     colnames(m)<-c('Instrument','ms')
     #head(m)
